8386671c1401ffcb34708d6a4117d126f2a850f3
garlic0x1 <garlic1@protonmail.com>
2024-06-02 00:15:14 -0500

    fix bug

--- examples/call-js-from-lisp.lisp
+++ examples/call-js-from-lisp.lisp
@@ -3,48 +3,46 @@
   (:export :run))
 (in-package :webui/examples/call-js-from-lisp)
 
+(defparameter *script* "
+let count = 0;
+function GetCount() {
+  return count;
+}
+function SetCount(number) {
+  document.getElementById('count').innerHTML = number;
+  count = number;
+}
+function AutoTest(number) {
+  setInterval(function(){ webui.call('MyButton1'); }, 10);
+}")
+
+(defparameter *html*
+  (hiccl:render nil
+    `(:html
+      (:head
+       (:script :src "webui.js")
+       (:title "Call JavaScript from C example"))
+      (:body
+       (:h1 "WebUI - Call JS from C") (:br)
+       (:h1 :id "count" "0") (:br)
+       (:button :id "MyButton1" "Manual Count") (:br)
+       (:button :id "MyTest" :onclick "AutoTest();" "Auto Count") (:br)
+       (:button :id "MyButton2" "Exit")
+       (:script (:raw ,*script*))))))
+
 (defun run ()
-  (let ((w (webui-new-window))
-        (html (hiccl:render nil
-                `(:html
-                  (:head
-                   (:script :src "webui.js")
-                   (:title "Call JavaScript from C example"))
-                  (:body
-                   (:h1 "WebUI - Call JS from C")
-                   (:br)
-                   (:h1 :id "count" "0")
-                   (:br)
-                   (:button :id "MyButton1" "Manual Count")
-                   (:br)
-                   (:button :id "MyTest" :onclick "AutoTest();" "Auto Count")
-                   (:br)
-                   (:button :id "MyButton2" "Exit")
-                   (:script
-                    (:raw
-                     (str:join
-                      "let count = 0;"
-                      "function GetCount() {"
-                      "  return count;"
-                      "}"
-                      "function SetCount(number) {"
-                      "  document.getElementById('count').innerHTML = number;"
-                      "  count = number;"
-                      "}"
-                      "function AutoTest(number) {"
-                      "  setInterval(function(){ webui.call('MyButton1'); }, 10);"
-                      "}"))))))))
+  (let ((w (webui-new-window)))
     (webui-bind w "MyButton1"
                 (lambda (ev)
                   (let* ((w (webui-event-window ev))
                          (c (webui-script w "return GetCount();" 1000)))
-                    (webui-run w (format nil "alert('~a');" c))
+                    ;; (webui-run w (format nil "alert('~a');" c))
                     (let ((n (1+ (parse-integer c))))
                       (webui-run w (format nil "SetCount(~a);" n))))))
     (webui-bind w "MyButton2"
                 (lambda (ev)
                   (declare (ignore ev))
                   (webui-exit)))
-    (webui-show w html)
+    (webui-show w *html*)
     (webui-wait)
     (webui-clean)))
